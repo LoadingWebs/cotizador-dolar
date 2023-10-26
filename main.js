92dd766f46c0c8b070744e7b8e154ecd83de940a
Daniela Cabred <danielacabred@gmail.com>
2023-10-25 21:02:07 -0300

    funciones

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function getDolarOficial(monto) {
     fetch ("https://dolarapi.com/v1/dolares/oficial")
     .then (respuesta => respuesta.json())
     .then (resultado => {
-        console.log(resultado);
+        return (resultado);
     })
     .catch (error => {
         console.log("error al consumir api dolar");
@@ -13,13 +13,15 @@ function getDolarBlue(monto) {
     fetch ("https://dolarapi.com/v1/dolares/blue")
     .then (respuesta => respuesta.json())
     .then (resultado => {
-        console.log(resultado);
+        return(resultado);
     })
     .catch (error => {
         console.log("error al consumir api dolar");
     })
 }
 
-getDolarBlue(100)
+function mostrarResultado(){
+    let importe = document.getElementById("monto").value
+} 
 
 
